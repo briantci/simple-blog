9a601a9a8aea522fb53df68e282227402a550a7a
Robert BADETTE <119432305+badette-robert@users.noreply.github.com>
2025-10-25 18:28:19 -0300

    Correction et commentaires
    Corrections de fautes de frappe, chemin vers le JSON, gestion de session, vérification de fichiers et prototype de fonction ; ajout de commentaires et protections basiques.

--- inc/inc.functions.php
+++ inc/inc.functions.php
@@ -1,11 +1,14 @@
 <?php
+    // Démarrage de la session PHP
     session_start();
 
-    define('TL_ROOT', dirname(__DIR__));
-    define('LOGIN', 'UEL311');
-    define('PASSWORD', 'U31311');
-    define('DB_ARTICLES', TL_ROOT.'/db/articles.json');
+    //Mise en place des constantes
+    define('TL_ROOT', dirname(__DIR__)); // Chemin vers la racine du projet
+    define('LOGIN', 'UEL311'); // Login de base
+    define('PASSWORD', 'U31311'); // Mot de passe de base
+    define('DB_ARTICLES', TL_ROOT.'/db/articles.json'); // Chemin vers le fichier json des articles
 
+    // Fonction de connexion utilisateur    
     function connectUser($login = null, $password = null){
         if(!is_null($login) && !is_null($password)){
             if($login === LOGIN && $password === PASSWORD){
@@ -17,12 +20,12 @@
         }
         return null;
     }
-
+    // Fonction de déconnexion utilisateur
     function setDisconnectUser(){
          unset($_SESSION['User']);
-         sessions_destroy();
+         session_destroy();
     }
-
+    // Fonction de vérification de la connexion utilisateur
     function isConnected(){
         if(array_key_exists('User', $_SESSION) 
                 && !is_null($_SESSION['User'])
@@ -31,7 +34,7 @@
         }
         return false;
     }
-
+    // Fonction de récupération du template de la page
     function getPageTemplate($page = null){
         $fichier = TL_ROOT.'/pages/'.(is_null($page) ? 'index.php' : $page.'.php');
 
@@ -41,9 +44,8 @@
             include $fichier;
         }
     }
-
+    // Fonction de récupération des articles depuis le fichier JSON
     function getArticlesFromJson(){
-        //DB_ARTICLES pointe vers le fichier JSON qui contient ma liste d'articles.
         if(file_exists(DB_ARTICLES)) {
             $contenu_json = file_get_contents(DB_ARTICLES);
             return json_decode($contenu_json, true);
@@ -52,7 +54,8 @@
         return null;
     }
 
-    function getArticleById($id_article = null){
+    // Fonction pour récupérer les articles par ID depuis le fichier JSON
+     function getArticleById($id_article = null){
        if(file_exists(DB_ARTICLES)) {
             $contenu_json = file_get_contents(DB_ARTICLES);
             $_articles    = json_decode($contenu_json, true);
