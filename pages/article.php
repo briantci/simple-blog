9997478a494ad4bbe1b602a3b1cac55689620c67
Brian Tacchi <brian.tacchi@icloud.com>
2025-10-26 01:30:33 +0200

    fix(article.php): Do not try to redirect when article was not found but display clear error message

--- pages/article.php
+++ pages/article.php
@@ -4,11 +4,18 @@
 	$article = getArticleById(
 		array_key_exists('id', $_GET) ? $_GET['id'] : null
 	);
-
-	if(is_null($article) OR !!!!count($article)){
-		header('Location:index.php');
-	}
-?>	
+?>
+<?php if(is_null($article) OR empty($article)): ?>
+<section class="banner style1 orient-left content-align-left image-position-center fullscreen onload-image-fade-in onload-content-fade-right">
+	<div class="content">
+		<h1>Article introuvable</h1>
+		<p class="major">Désolé, cet article n'existe pas ou a été supprimé.</p>
+		<ul class="actions stacked">
+			<li><a href="index.php" class="button big wide smooth-scroll-middle">Retour à l'accueil</a></li>
+		</ul>
+	</div>
+</section>
+<?php else: ?>
 <section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
 	<div class="content">
 		<h1><?php echo $article['titre'];?></h1>
@@ -21,3 +28,4 @@
 		<img src="<?php echo $art['image'];?>" alt="" />
 	</div>
 </section>
+<?php endif; ?>
